8e30301b216c79476215a97c5de442c0b2d283df
anton <tosha.fedotov.2000@gmail.com>
2020-04-11 22:04:12 +0500

    init commit

---
+++ src/main/java/ru/perlhackers/fanfile/FanfileApplication.java
@@ -0,0 +1,13 @@
+package ru.perlhackers.fanfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/perlhackers/fanfile/FanfileApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.perlhackers.fanfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
